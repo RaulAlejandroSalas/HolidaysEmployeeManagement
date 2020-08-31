90943b480d73f8fe5b1b8288bb82136a76430500
unknown <RSAL182@cc-college.gecs.loc>
2020-08-31 09:35:24 +0200

    first commit

---
+++ src/main/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Lic.Raul Alejandro Salas Texido
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("App Up!!");
+
+	}
+
+}
